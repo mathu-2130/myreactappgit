a3b6532c32129d6afefb5cc191ccceb7153bf147
Mathumathi <mathumathir2196.com>
2023-08-04 16:50:15 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         <h1> Welcome ....this is madhu</h1>
+         <h4>Learn react in udemy</h4>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
